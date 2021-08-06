d901b9c298f620f4517fbfa77d1f14d8113cb08b
Sindhuphython <sindhubarathiak@gmail.com>
2021-08-06 13:58:23 +0530

    resubmit completed

--- 5august-day14assignments/Agust5_Sindhu14_assignment/pp.py
+++ 5august-day14assignments/Agust5_Sindhu14_assignment/pp.py
@@ -1,30 +1,32 @@
-import multiprocessing,time
-def primeNumbers():
-    for i in range(2,500):
-        time.sleep(3)
-        if i>1:
-            for j in range(2,i):
-                if i%j==0:
-                    break
-            else:
-                print("primenumbers:",i)
-def palindrome():
-    for i in range(10,500):
-        time.sleep(3)
-        temp=i
-        rev=0
-        while(temp>0):
-            rem=temp%10
-            rev=(rev*10)+rem
-            temp=temp//10
-            if(i==rev):
-                print("palindrome:",i)
+import multiprocessing,time,logging
+try:
+    def primeNumbers():
+        for i in range(2,500):
+            #tine.sleep(1)
+            if i>1:
+                for j in range(2,i):
+                    if i%j==0:
+                        break
+                    else:
+                        print("primenumbers:",i,end='')
+    def palindrome():
+        for i in range(10,500):
+            temp=i
+            rev=0
+            while(temp>0):
+                rem=temp%10
+                rev=(rev*10)+rem
+                temp=temp//10
+                if(i==rev):
+                    print("palindrome:",i,end='')
 
-if(__name__=="__main__"):
-p1=threading.Thread(target=primeNumbers)
-p2=threading.Thread(target=palindrome)
-p1.start()
-p2.start()
-p1.join()
-p2.join()
-print("..........")
+    if __name__ == "__main__":
+        p1=multiprocessing.Process(target=primeNumbers)
+        p2=multiprocessing.Process(target=palindrome)
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+        print("..........")
+except Exception:
+    logging.error("Something Wrong!")
--- 5august-day14assignments/Agust5_Sindhu14_assignment/tt.py
+++ 5august-day14assignments/Agust5_Sindhu14_assignment/tt.py
@@ -1,29 +1,32 @@
-import threading,time
-def primeNumbers():
-    for i in range(2,500):
-        time.sleep(3)
-        if i>1:
-            for j in range(2,i):
-                if i%j==0:
-                    break
-            else:
-                print("primenumbers:",i)
-def palindrome():
-    for i in range(10,500):
-        time.sleep(3)
-        temp=i
-        rev=0
+import threading,time,logging
+try:
+    def primeNumbers():
+        for i in range(2,500):
+            # time.sleep(3)
+            if i>1:
+                for j in range(2,i):
+                    if i%j==0:
+                        break
+                    else:
+                        print("primenumbers:",i,end='')
+    def palindrome():
+        for i in range(10,500):
+            # time.sleep(3)
+            temp=i
+            rev=0
         while(temp>0):
             rem=temp%10
             rev=(rev*10)+rem
             temp=temp//10
             if(i==rev):
-                print("palindrome:",i)
-        
-t1=threading.Thread(target=primeNumbers)
-t2=threading.Thread(target=palindrome)
-t1.start()
-t2.start()
-t1.join()
-t2.join()
-print("..........")
+                print("palindrome:",i,end='')
+    if(__name__=='__main__'):
+        t1=threading.Thread(target=primeNumbers)
+        t2=threading.Thread(target=palindrome)
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+        print("..........")
+except Exception:
+    logging.error("Something Wrong!")
