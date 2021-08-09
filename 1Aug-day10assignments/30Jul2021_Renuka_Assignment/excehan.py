97ec947383cc645b34425e5fc5d202436ccfa013
AadrikaK <aadrikakhare99@gmail.com>
2021-08-09 09:32:48 +0530

    6th august

--- 1Aug-day10assignments/30Jul2021_Renuka_Assignment/excehan.py
+++
@@ -1,9 +0,0 @@
-try:
-    n1=int(input("enter a numbers"))
-    n2=int(input("enter a number "))
-    div=n1/n2
-    print(div)
-except ZeroDivisionError:
-    print("ArithmeticException")
-except ValueError:
-    print("number should be entered")
